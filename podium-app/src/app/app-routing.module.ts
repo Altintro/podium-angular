289cede18171807b0b4b76478a39487b0b0c5dd8
Tomas Moyano <tomas@hella.ventures>
2018-04-22 12:57:13 +0200

    adding Game and Sport models

--- podium-app/src/app/app-routing.module.ts
+++ podium-app/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GamesComponent } from './games/games.component'
+import { DownloadComponent } from './download/download.component'
+import { TeamComponent } from './team/team.component'
 
 const routes: Routes = [
-  { path: 'games', component: GamesComponent }
+  { path: 'games', component: GamesComponent },
+  { path: 'download', component: DownloadComponent},
+  { path: 'team', component: TeamComponent}
+
 ]
 
 @NgModule({
--- podium-app/src/model/Game.ts
+++ podium-app/src/model/Game.ts
@@ -1,4 +1,19 @@
+import { Sport } from "./Sport";
+
 export class Game {
     id: number;
     name: String;
+    sport: Sport;
+    description: String;
+    /*tournament: Tournament;
+    participants: [Team];
+    wins: Team;
+    loses: Team;*/
+    open: Boolean;
+    concluded: Boolean;
+    date: Date;
+    latitude: Number;
+    longitude: Number;
+    modality: String;
+    levelAverage: String
 }
